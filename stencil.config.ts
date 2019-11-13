1b07e4dae70cbabe244593d1f9af01fc1a400ea1
Jeffrey Bosch <Jefiozie@users.noreply.github.com>
2019-11-13 12:43:25 +0100

    ci: add initial setup for ci and pr's (#13)
    * ci: add initial setup for ci and pr's
    
    * doc: change pull request template
    
    * ci: adjust to run tests

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,12 +3,15 @@ import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 export const config: Config = {
   namespace: 'xlayers-lite',
-  testing:{
+  testing: {
     moduleFileExtensions: ['json', 'ts', 'tsx', 'js', 'css'],
     moduleNameMapper: {
       '@xlayers/css-blocgen': '<rootDir>/src/@xlayers/css-blocgen/',
-      '@xlayers/sketch-lib' : '<rootDir>/src/@xlayers/sketch-lib/',
-      '@xlayers/svg-blocgen' : '<rootDir>/src/@xlayers/svg-blocgen/',
+      '@xlayers/sketch-lib': '<rootDir>/src/@xlayers/sketch-lib/',
+      '@xlayers/svg-blocgen': '<rootDir>/src/@xlayers/svg-blocgen/',
+      "@xlayers/core": "<rootDir>src/@xlayers/core/index",
+      "@xlayers/sketch-ingestor": "<rootDir>src/@xlayers/sketch-ingestor/index",
+      "@xlayers/web-blocgen": "<rootDir>src/@xlayers/web-blocgen/index",
     }
   },
   outputTargets: [
